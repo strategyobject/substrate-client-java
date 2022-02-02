0d85c5625c3c2c10bb0edcd4d8d6bec38d632674
Alexander Kalankhodzhaev <kalansoft@gmail.com>
2022-02-02 15:22:36 +0300

    implement toString() for AccountId

--- rpc/rpc-types/src/main/java/com/strategyobject/substrateclient/rpc/types/AccountIdEncoder.java
+++ rpc/rpc-types/src/main/java/com/strategyobject/substrateclient/rpc/types/AccountIdEncoder.java
@@ -1,9 +1,7 @@
 package com.strategyobject.substrateclient.rpc.types;
 
 import com.google.common.base.Preconditions;
-import com.strategyobject.substrateclient.crypto.ss58.SS58Codec;
 import com.strategyobject.substrateclient.rpc.core.EncoderPair;
-import com.strategyobject.substrateclient.rpc.core.MetadataRegistry;
 import com.strategyobject.substrateclient.rpc.core.RpcEncoder;
 import com.strategyobject.substrateclient.rpc.core.annotations.AutoRegister;
 
@@ -13,6 +11,6 @@ public class AccountIdEncoder implements RpcEncoder<AccountId> {
     public Object encode(AccountId source, EncoderPair<?>... encoders) {
         Preconditions.checkArgument(encoders == null || encoders.length == 0);
 
-        return SS58Codec.encode(source.getData(), MetadataRegistry.getInstance().getSS58AddressFormat());
+        return source.toString();
     }
 }
