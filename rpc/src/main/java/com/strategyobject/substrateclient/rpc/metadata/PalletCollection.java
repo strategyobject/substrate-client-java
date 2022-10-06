94ea46c31ed48b65ee5090e5d4da1227023b5bf5
Peter Frank <peter.frank@unfinished.com>
2022-10-06 11:52:21 -0700

    Now indexing modules by their ... index
    Previously it was using an index of an array to dereference to a module, this is no good because there are now gaps between the numbers so the original assumption of the library does not hold

--- rpc/src/main/java/com/strategyobject/substrateclient/rpc/metadata/PalletCollection.java
+++ rpc/src/main/java/com/strategyobject/substrateclient/rpc/metadata/PalletCollection.java
@@ -1,12 +1,14 @@
 package com.strategyobject.substrateclient.rpc.metadata;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.FluentIterable;
 import lombok.NonNull;
 
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class PalletCollection {
-    private final Pallet[] pallets;
+    private final Map<Integer, Pallet> palletsIndex;
 
     public PalletCollection(Pallet @NonNull ... pallets) {
         Preconditions.checkArgument(pallets.length > 0);
@@ -15,14 +17,14 @@ public class PalletCollection {
                 IntStream.range(1, pallets.length)
                         .allMatch(i -> pallets[i].getIndex() == pallets[i - 1].getIndex() + 1));*/
 
-        this.pallets = pallets;
+        palletsIndex = FluentIterable.from(pallets).uniqueIndex(Pallet::getIndex);
     }
 
     public Pallet get(int index) {
-        return pallets[index];
+        return palletsIndex.get(index);
     }
 
     public int size() {
-        return pallets.length;
+        return palletsIndex.size();
     }
 }
