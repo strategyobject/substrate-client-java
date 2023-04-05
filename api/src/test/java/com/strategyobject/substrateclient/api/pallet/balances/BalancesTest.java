bcb8268475b84907d41f3595d1227ca43a730f12
Peter Frank <peter.frank@unfinished.com>
2023-04-05 13:30:44 -0700

    Updated to latest frequency

--- api/src/test/java/com/strategyobject/substrateclient/api/pallet/balances/BalancesTest.java
+++ api/src/test/java/com/strategyobject/substrateclient/api/pallet/balances/BalancesTest.java
@@ -90,7 +90,7 @@ class BalancesTest {
     }
 
     private Extrinsic<?, ?, ?, ?> createBalanceTransferExtrinsic(BlockHash genesis) {
-        val specVersion = 21;
+        val specVersion = 24;
         val txVersion = 1;
         val moduleIndex = (byte) 10;
         val callIndex = (byte) 0;
--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,7 +1,7 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.3.0";
+    public static final String CURRENT_VERSION = "v1.4.0";
 
     private FrequencyVersion() {
     }
